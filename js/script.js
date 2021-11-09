342c0b4341a44a3edf6eaca4d73cebe2bdb39bc7
Artem <turbolaft@gmail.com>
2021-11-09 12:05:08 +0200

    Commmit

--- js/script.js
+++ js/script.js
@@ -3,7 +3,12 @@ const swiper1 = document.querySelector('.mySwiper'),
 			burger = document.querySelector('.burger'),
 			menuClose = document.querySelector('.menu__close'),
 			menu = document.querySelector('.menu'),
-			playButtonsFirst = document.querySelectorAll('.main-slider__play');
+			playButtonsFirst = document.querySelectorAll('.main-slider__play'),
+			featured = document.querySelector('.featured'),
+			featuredBurger = document.querySelector('.featured__burger--show'),
+			featuredTriller = document.querySelector('.featured__burger'),
+			featuredStories = document.querySelector('.featured__stories'),
+			closeFeatured = document.querySelector('.close__featured--stories');
 
 let Swiper1 = new Swiper(swiper1, {
 	centeredSlides: true,
@@ -27,6 +32,15 @@ let Swiper2 = new Swiper(swiper2, {
 	},
 });
 
+Swiper2.on('transitionEnd', function () {
+	let videos = document.querySelectorAll('.first__slider video');
+	videos.forEach(el => {
+		el.pause();
+		el.currentTime = 0;
+	});
+	playButtonsFirst.forEach(el => el.style.display = 'block');
+});
+
 burger.addEventListener('click', () => {
 	showMyMenu(menu);
 });
@@ -37,22 +51,51 @@ menuClose.addEventListener('click', () => {
 
 function showMyMenu(section) {
 	section.classList.remove('d-none');
-	section.classList.add('d-show');
+	section.classList.add('show');
 	document.body.style.overflow = 'hidden';
 }
 
 function closeMyMenu(section) {
-	section.classList.remove('d-show');
+	section.classList.remove('show');
 	section.classList.add('d-none');
 	document.body.style.overflow = '';
 }
 
+document.addEventListener('keydown', e => {
+	if (e.code === "Escape" && menu.classList.contains("show")) {
+		closeMyMenu(menu);
+	}
+});
+
 playButtonsFirst.forEach(el => {
 	el.addEventListener('click', e => {
-		let video = e.currentTarget.closest('.swiper-wrapper').querySelector('video');
+		let video = e.currentTarget.closest('.main-slider__media').querySelector('video');
 		video.play();
+		e.currentTarget.style.display = 'none';
 		setTimeout(() => {
 			video.volume = 1;
 		}, 1000);
 	});
+});
+
+function showMyFeaturedMenu(section, menu) {
+	section.classList.remove('d-hide__right');
+	menu.classList.remove('d-show__right');
+	menu.classList.add('d-hide__right');
+	section.classList.add('d-show__right');
+}
+
+featuredTriller.addEventListener('click', () => {
+	showMyFeaturedMenu(featuredStories, featuredBurger);
+});
+
+function hideMyFeaturedMenu(section, menu) {
+	section.classList.remove('d-show__right');
+	menu.classList.remove('d-hide__right');
+	menu.classList.add('d-show__right');
+	section.classList.add('d-hide__right');
+}
+
+closeFeatured.addEventListener('click', () => {
+	hideMyFeaturedMenu(featuredStories, featuredBurger);
 });
